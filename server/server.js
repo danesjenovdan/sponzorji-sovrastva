9aefd279f29c66d5f2abbacb6e66f747a87022ea
LepkoQQ <lepko.san@gmail.com>
2018-10-30 18:01:53 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -2,46 +2,45 @@ const express = require('express');
 const app = express();
 const cheerio = require('cheerio');
 const fs = require('fs');
-const request = require('request-promise-native');
-const ejs = require('ejs');
-const webshot = require('webshot');
+const _ = require('lodash');
 
-let ogCount = 0;
+const clientPath = `${__dirname}/../pokrovitelji`;
+const razgledniceJson = require('../pokrovitelji/src/assets/razglednice.json')
+const vueConfig = require('../pokrovitelji/vue.config');
 
 // Load index.html
-const indexFile = fs.readFileSync(`${__dirname}/../pokrovitelji/dist/index.html`, 'utf8', (err, file) => err ? reject(err) : resolve(file));
-const indexHtml = indexFile.toString();
+const indexHtml = fs.readFileSync(`${clientPath}/dist/index.html`, 'utf8');
+const baseUrl = vueConfig.baseUrl;
+const razglednice = razgledniceJson.razglednice;
 
-// DO THIS FOR EVERY SPECIFIC SPONSOR
-app.get('/pokrovitelji/triglav', async (req, res) => {
+razglednice.forEach((rzg) => {
+  app.get(`${baseUrl}${rzg.link}`, async (req, res) => {
+    try {
+      const $ = cheerio.load(indexHtml);
+      $('.removeme').remove();
 
-  try {
-    const $ = cheerio.load(indexHtml);
+      $('title').text(`Ustavimo botre sovraštva`);
+      $('head').append(`
+        <meta property="og:url" content="https://danesjenovdan.si${baseUrl}${rzg.link}">
+        <meta property="og:title" content="Ustavimo botre sovraštva">
+        <meta name="twitter:title" content="Ustavimo botre sovraštva">
 
-    $('title').text('TRIGLAV OG TEXT');
-    $('.removeme').remove();
-    $('head').append(`
-      <meta property="og:url" content="https://danesjenovdan.si/pokrovitelji/triglav" />
-      <meta property="og:title" content="TRIGLAV TITLE" />
-      <meta name="twitter:title" content="TRIGLAV TITLE">
+        <meta property="og:image" content="https://danesjenovdan.si${baseUrl}razglednice/${rzg.imgSrc}">
+        <meta name="twitter:image" content="https://danesjenovdan.si${baseUrl}razglednice/${rzg.imgSrc}">
 
-      <meta property="og:image" content="TRIGLAV IMAGE" />
-      <meta name="twitter:image" content="TRIGLAV IMAGE">
+        <meta property="og:description" content="${rzg.tweet}">
+        <meta name="twitter:description" content="${rzg.tweet}">
+      `);
 
-      <meta property="og:description" content="TRIGLAV DESCRIPTION" />
-      <meta name="twitter:description" content="TRIGLAV DESCRIPTION">
-    `);
-
-    res.send($.html());
-
-  } catch (err) {
-    console.log(err);
-    res.status(400).send('Something went wrong');
-  }
-
+      res.send($.html());
+    } catch (err) {
+      console.log(err);
+      res.status(400).send('Something went wrong');
+    }
+  });
 });
 
-app.use('/pokrovitelji', express.static(`${__dirname}/../pokrovitelji/dist`));
+app.use(`${baseUrl}`, express.static(`${clientPath}/dist`));
 
 app.listen(7001, () => {
   console.log('Server listening on port 7001');
