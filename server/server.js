a7138f1a9b7535dffd6eda7c5c2a5aa83a0c42c8
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-11-05 15:27:43 +0100

    final OG fixes

--- server/server.js
+++ server/server.js
@@ -21,15 +21,16 @@ razglednice.forEach((rzg) => {
 
       $('title').text(`Ustavimo botre sovraštva`);
       $('head').append(`
+        <title>${rzg.title}</title>
         <meta property="og:url" content="https://danesjenovdan.si${baseUrl}${rzg.link}">
-        <meta property="og:title" content="Ustavimo botre sovraštva">
-        <meta name="twitter:title" content="Ustavimo botre sovraštva">
+        <meta property="og:title" content="${rzg.title}">
+        <meta name="twitter:title" content="${rzg.title}">
 
         <meta property="og:image" content="https://danesjenovdan.si${baseUrl}razglednice/${rzg.imgSrc}">
         <meta name="twitter:image" content="https://danesjenovdan.si${baseUrl}razglednice/${rzg.imgSrc}">
 
-        <meta property="og:description" content="${rzg.tweet}">
-        <meta name="twitter:description" content="${rzg.tweet}">
+        <meta property="og:description" content="${rzg.description}">
+        <meta name="twitter:description" content="${rzg.description}">
       `);
 
       res.send($.html());
